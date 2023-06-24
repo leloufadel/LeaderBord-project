aa2a9718ae11f68a13ed65f0e65b6ba01d368758
leloufadel <leloufadel@gmail.com>
2023-06-24 13:49:00 +0300

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Output Management',
+    template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+   clean: true,
+  },
+};
