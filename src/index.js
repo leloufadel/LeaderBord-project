558d673bdce658a5fc9377988b0d3b7901c98226
leloufadel <leloufadel@gmail.com>
2023-06-24 14:06:44 +0300

    leaderboard setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import './style.css';
+import populateScores from './modules/populateScores.js';
+import { submitScore } from './modules/domElements.js';
+
+submitScore.addEventListener('click', populateScores);
